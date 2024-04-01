4e76c6e4f857e93584188850e1e04fcd3c5e910c
imamtagupta <mamtag962000@gmail.com>
2024-04-01 22:55:12 +0530

    58. Length of Last Word

---
+++ lenth_of_last_word.py
@@ -0,0 +1,11 @@
+#############################################
+# 58. Length of Last Word                  #
+#############################################
+
+# leetcode link: https://leetcode.com/problems/length-of-last-word/description/
+
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
