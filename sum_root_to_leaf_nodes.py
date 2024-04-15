a7f50b6bc1b1308f4712464af6a1405592227f46
imamtagupta <mamtag962000@gmail.com>
2024-04-15 08:22:22 +0530

    129. Sum Root to Leaf Numbers

--- sum_root_to_leaf_nodes.py
+++ sum_root_to_leaf_nodes.py
@@ -14,3 +14,20 @@ class Solution:
         if not root.left and not root.right:
             return sum
         return self.sumNumbers(root.left, sum) + self.sumNumbers(root.right, sum)
+    
+
+# A better time complexity similar solution:
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(node, path: str):
+            nonlocal ans
+            path = path + str(node.val)
+            if not node.left and not node.right:
+                ans += int(path)
+            else:
+                if node.left: dfs(node.left, path)
+                if node.right: dfs(node.right, path)
+        dfs(root, "")
+        return ans
