3eea123786dbd002b00cdb4f08a448680d2ad25a
imamtagupta <mamtag962000@gmail.com>
2024-05-09 10:05:05 +0530

    3075. Maximize Happiness of Selected Children

---
+++ maximize_happiness_of_selected_children.py
@@ -0,0 +1,23 @@
+
+###############################################
+# 3075. Maximize Happiness of Selected Children #
+###############################################
+
+# leetcode link: https://leetcode.com/problems/maximize-happiness-of-selected-children/
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        i = 0
+        res = 0
+
+        while k > 0:
+            happiness[i] = max(happiness[i] - i, 0)
+            res += happiness[i]
+            i += 1
+            k -= 1
+
+        return res
+
+
+        
